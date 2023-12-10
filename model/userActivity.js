2969e29c79abf087e60ec74b58c8ee498d2add70
adityagurnani22 <gurnani.ad@northeastern.edu>
2023-12-09 23:54:17 -0500

    db cleanup

--- model/userActivity.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const userActivitySchema = require('../schema/userActivitySchema');
-
-const UserActivity = mongoose.model('UserActivity', userActivitySchema);
-
-module.exports = UserActivity;
--- model/userDetails.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const userDetailsSchema = require('../schema/userDetailsSchema');
-
-const UserDetails = mongoose.model('UserDetails', userDetailsSchema);
-
-module.exports = UserDetails;
